3e195b8cc5451f930f0d95466a01b08b04716d06
MoniNog <nogueiramon@gmail.com>
2025-05-11 21:01:15 +0200

    correction kind of token

--- mini/sh/src/exec/core_exec.c
+++ mini/sh/src/exec/core_exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   core_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lylrandr <lylrandr@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: monoguei <monoguei@student.lausanne42.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 16:02:20 by monoguei          #+#    #+#             */
-/*   Updated: 2025/05/11 18:59:44 by lylrandr         ###   ########.fr       */
+/*   Updated: 2025/05/11 21:01:02 by monoguei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,32 @@ int	kind_of_token(t_data *data, t_input *input)
 	char	*input_str;
 
 	input_str = input->token;
-	if (ft_strncmp_end(input_str, "echo", 4) == 0)
+	if (ft_strncmp_end(input_str, "echo", 5) == 0)
 	{
 		b_echo(data);
 		return (0);
 	}
-	if (ft_strncmp_end(input_str, "exit", 4) == 0)
+	if (ft_strncmp_end(input_str, "exit", 5) == 0)
 	{
 		b_exit(data);
 		return (0);
 	}
-	else if (ft_strncmp_end(input_str, "cd", 2) == 0)
+	else if (ft_strncmp_end(input_str, "cd", 3) == 0)
 	{
 		b_cd(data);
 		return (0);
 	}
-	else if (ft_strncmp_end(input_str, "pwd", 3) == 0)
+	else if (ft_strncmp_end(input_str, "pwd", 4) == 0)
 	{
 		b_pwd(data);
 		return (0);
 	}
-	else if (ft_strncmp_end(input_str, "env", 3) == 0)
+	else if (ft_strncmp_end(input_str, "env", 4) == 0)
 	{
 		b_env(data);
 		return (0);
 	}
-	else if (ft_strncmp_end(input_str, "unset", 5) == 0)
+	else if (ft_strncmp_end(input_str, "unset", 6) == 0)
 	{
 		b_unset(data);
 		return (0);
